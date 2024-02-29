e2f2fe85effad6b104aa4234c600b0588203ef51
Arnab Roy <royarnab056@gmail.com>
2024-02-29 13:04:55 +0530

    commiting...

---
+++ src/lib/redis.ts
@@ -0,0 +1,7 @@
+import { Redis } from '@upstash/redis'
+  
+export const redis = new Redis({
+  url: 'https://us1-allowing-baboon-37107.upstash.io',
+  token: 'AZDzASQgYjRiMGZjZWQtZjdiOC00MGY0LWI1MDEtYTExZmNlOWJjNzY2MmIyNWEyNWI1Nzg2NDcxYWJkZDY4OTNhY2EzZTk4Y2Y=' ,
+})
+  
